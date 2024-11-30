5090c7ce5d8dd709ef2d25e1ffcdf00e0b3a4885
Leticia-SS <leticia.saraiva013@gmail.com>
2024-11-30 14:01:25 -0300

    feat: create stack navigation

--- TP3/app/index.tsx
+++ TP3/app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router'
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <Link href='/profile' style={styles.text}>Perfil</Link>
     </View>
   );
 }
