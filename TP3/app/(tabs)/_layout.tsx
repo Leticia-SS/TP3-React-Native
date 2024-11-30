9986b199e4a66b996f24e8347e1047145f6987b3
Leticia-SS <leticia.saraiva013@gmail.com>
2024-11-30 15:53:08 -0300

    style: ajust rootLayout and tabLayout styleSheet

--- TP3/app/(tabs)/_layout.tsx
+++ TP3/app/(tabs)/_layout.tsx
@@ -1,10 +1,24 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons"
 
 export default function TabsLayout() {
   return (
-  <Tabs>
-    <Tabs.Screen name="index" options={{ title: 'Home', headerShown: false }} />
-    <Tabs.Screen name="profile" options={{ title: 'Perfil', headerShown: false }} />
+  <Tabs 
+    screenOptions={{
+      tabBarActiveTintColor: '#8c52ff',
+      tabBarActiveBackgroundColor: '#140024',
+      headerShadowVisible: false,
+      headerTintColor: '#8c52ff',
+      headerStyle: {
+        backgroundColor: '#140024'
+      },
+      tabBarStyle: {
+        backgroundColor: '#0d0018'
+      }
+    }}
+  >
+    <Tabs.Screen name="index" options={{ title: 'Home', headerShown: false, tabBarIcon: ({focused, color}) => <Ionicons name={ focused ? "home-sharp" : "home-outline"} size={30} color={color} /> }} />
+    <Tabs.Screen name="profile" options={{ title: 'Perfil', headerShown: false, tabBarIcon: ({focused, color}) => <Ionicons name={ focused ? "id-card-sharp" : "id-card-outline"} size={30} color={color} /> }} />
     <Tabs.Screen name="not-found" options={{  headerShown: false }} />
   </Tabs>
   );
--- TP3/app/_layout.tsx
+++ TP3/app/_layout.tsx
@@ -1,14 +1,20 @@
 import { Stack } from "expo-router";
 import { LogBox } from "react-native"
+import { StatusBar } from "expo-status-bar"
 
 LogBox.ignoreAllLogs(true)
 
 export default function RootLayout() {
   return (
-  <Stack>
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    <Stack.Screen name="not-found" options={{ headerShown: false }} />
-  </Stack>);
+    <>
+    <StatusBar style="light" /> 
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="not-found" options={{ headerShown: false }} />
+    </Stack>
+  </>
+  );
 }
 
 // usar headerLeft: () => <></>  evita que o botão apareca em cima na tela
+// statusBar é a barra que tem os status do celular, de hora bateria etc
